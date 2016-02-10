12073478fcd1dd289d810bf4d35cadf4e72975da
bredman <bredman@slack-corp.com>
2016-02-10 13:06:03 -0800

    Initial commit of test script and simple driver

---
+++ array-append-test.php
@@ -0,0 +1,58 @@
+<?php
+
+error_reporting(E_ALL & ~E_NOTICE);
+
+$longopts = array(
+	"append-type:",
+	"num-elements:",
+	"element-size:",
+);
+
+$options = getopt("", $longopts);
+
+$append_type = $options['append-type'];
+
+if (!isset($append_type)){
+	echo "must define append type\n";
+	exit;
+}
+
+$num_elements = $options['num-elements'] ?: 1000;
+$element_size = $options['element-size'] ?: 100;
+
+$test_arr1 = generate_test_data($num_elements, $element_size);
+$test_arr2 = generate_test_data($num_elements, $element_size);
+
+$start_time = microtime(true);
+$result = call_user_func("test_{$append_type}", $test_arr1, $test_arr2);
+$end_time = microtime(true);
+
+$run_time_ms = ($end_time - $start_time) *1000;
+echo "{$append_type},{$num_elements},{$element_size}," . sprintf("%.3f", $run_time_ms) . "\n";
+
+function generate_test_data($count, $element_size){
+	$test_data = array();
+	$is_secure = false;
+	while($count > 0){
+		$element_string = bin2hex(openssl_random_pseudo_bytes(ceil($element_size / 2)));
+		if ($element_size % 2 != 0) $element_string = substr($element_string, 1);
+
+		$test_data[] = $element_string;
+		$count--;
+	}
+
+	return $test_data;
+}
+
+function test_builtin_append($arr1, $arr2){
+	foreach ($arr2 as $arr2_val){
+		$arr1[] = $arr2_val;
+	}
+
+	return $arr1;
+}
+
+function test_array_merge($arr1, $arr2){
+	$result = array_merge($arr1, $arr2);
+	return $result;
+}
---
+++ test-by-list-size-driver.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+set -e
+
+REPO_PATH=`git rev-parse --show-toplevel`
+TEST_FILE=${REPO_PATH}/php-array-append-test.php
+
+echo "append_type,num_elements,element_size,time"
+
+for APPEND_TYPE in "array_merge" "builtin_append"; do
+    for LIST_SIZE in "1000" "2000" "4000" "8000" "16000" "32000"; do
+        for TRIAL_COUNT in `seq 1 100`; do
+            php $TEST_FILE --num-elements $LIST_SIZE --append-type $APPEND_TYPE
+        done
+    done
+done
